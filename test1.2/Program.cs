9d4f72b233af49623c15f418424d7aebd8976c9c
romap <romap@DESKTOP-E4D993R>
2022-06-10 22:04:52 +0300

    Добавьте файлы проекта.

---
+++ test1.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int coutCars = 5;
+            string myName = "Roman";
+            ushort age = 23;
+            bool isLight = true;
+            double numberPI = 3.14;
+            char buttonSpace = ' ';
+            byte sizepick = 234;
+            long numberPhone = 88005553535;
+            float litersGasoline = 30.5f;
+            ulong balanceRubles = 999999999999;
+        }
+    }
+
